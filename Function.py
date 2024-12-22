b96732f5bc35e2bf89417643a2e5e883c2af9512
jamuojisan <kage0427@gmail.com>
2024-12-22 12:27:33 +0900

    多変数入出力に対応

--- Function.py
+++ Function.py
@@ -33,14 +33,15 @@ class Variable:
 
 class Function:
     # 関数
-    def __call__(self, input: Variable):
-        x = input.data
-        y = self.forward(x)
-        output = Variable(as_array(y))
-        output.set_creator(self) # 出力変数に生みの親を覚えさせる。
-        self.input = input
-        self.output = output # 出力も覚える。
-        return output
+    def __call__(self, inputs: Variable):
+        xs = [x.data for x in inputs.data]
+        ys = self.forward(xs)
+        outputs = [Variable(as_array(y)) for y in ys]
+        for output in outputs:
+            output.set_creator(self) # 出力変数に生みの親を覚えさせる。
+        self.inputs = inputs
+        self.outputs = outputs # 出力も覚える。
+        return outputs
     
     def forward(self, x):
         # Functionクラスのforwardクラスを使った人に、継承先で実装すべきなことを教えてあげる
@@ -74,25 +75,9 @@ def square(x):
 def exp(x):
     return Exp()(x)
 
-# x = Variable(np.array(0.5))
-# y = square(exp(square(x)))
-# y.backward()
-# print(x.grad)
-
-
 def numerical_diff(f, x, eps=1e-4):
     x0 = Variable(np.asarray(x.data - eps))
     x1 = Variable(np.asarray(x.data + eps))
     y0 = f(x0)
     y1 = f(x1)
     return (y1.data - y0.data) / (2 * eps)
-
-# def f(x):
-#     A = Square()
-#     B = Exp()
-#     C = Square()
-#     return C(B(A(x)))
-
-x = Variable(np.array(0.5))
-dy = numerical_diff(square, x)
-print(dy)
