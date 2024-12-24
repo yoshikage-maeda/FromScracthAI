8e23bdc9ee8ee1f7f585b1a4a362ce8d1cf00968
jamuojisan <kage0427@gmail.com>
2024-12-24 14:29:29 +0900

    minor

--- dezero/__init__.py
+++ dezero/__init__.py
@@ -1,7 +1,7 @@
-from dezero.core_simple import Variable, Function
-from dezero.core_simple import using_config, no_grad
-from dezero.core_simple import as_array, as_variable
-from dezero.core_simple import setup_variable
-from dezero.core_simple import CustomTapple
+from dezero.core import Variable, Function
+from dezero.core import using_config, no_grad
+from dezero.core import as_array, as_variable
+from dezero.core import setup_variable
+from dezero.core import CustomTapple
 
 setup_variable()
