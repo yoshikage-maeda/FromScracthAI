861bcecf9a6e88ba8aca6a292b8f93ee633965bb
jamuojisan <kage0427@gmail.com>
2024-12-26 21:15:02 +0900

    add

--- Function.py
+++ Function.py
@@ -1,119 +1,91 @@
 import numpy as np
-import heapq
+from dezero import Variable, Function
+from dezero.utils import plot_dot_graph
+import dezero.functions  as F
 
-def as_array(x):
-    if np.isscalar(x):
-        return np.array(x)
-    return x
+if '__file__' in globals():
+    import sys, os
+    sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+def sphere(x, y):
+    z = x ** 2 + y ** 2
+    return z
 
-class CustomTapple:
-    def __init__(self, priority, data):
-        self.priority = priority # 比較に使う値
-        self.data = data # 比較に使わない
-    
-    def __lt__(self, other):
-        return self.priority < other.priority # priorityだけで比較
+def matyas(x, y):
+    z = 0.26 * (x ** 2 + y ** 2) - 0.48 * x * y
+    return z
 
-class Variable:
-    # 変数
-    def __init__(self, data):
-        if data is not None:
-            if not isinstance(data, np.ndarray):
-                raise TypeError(f"{type(data)} is not supported")
-        self.data = data
-        self.grad = None # 微分が計算されたときにその値を設定する。
-        self.creator = None
-        self.generation = 0
+def goldstein(x, y):
+    z = (1 + (x + y + 1)**2 * (19 - 14*x + 3*x**2 - 14*y + 6*x*y + 3*y**2)) * (30 + (2*x - 3*y)**2  * (18 -32*x + 12*x**2 + 48*y - 36*x*y + 27*y**2))
+    return z
 
-    
-    def set_creator(self, func):
-        self.creator = func
-        self.generation = func.generation + 1
-    
-    def backward(self):
-        if self.grad is None:
-            self.grad = np.ones_like(self.data)
-        funcs = []
-        seen_set = set()
-        def add_func(f):
-            if f not in seen_set:
-                heapq.heappush(funcs, CustomTapple(-f.generation, f))
-                seen_set.add(f)
-        add_func(self.creator)
-        while funcs:
-            f_info = heapq.heappop(funcs)
-            f = f_info.data  # 関数の情報を取得
-            
-            gys = [output.grad for output in f.outputs]
-            gxs = f.backward(*gys)
-            if not isinstance(gxs, tuple):
-                gxs = (gxs,)  # 1要素のタプルに変換する。
-            
-            for x, gx in zip(f.inputs, gxs):
-                if x.grad is None:
-                    x.grad = gx
-                else:
-                    x.grad = x.grad + gx
-
-                if x.creator is not None:
-                    add_func(x.creator)
-    def cleargrad(self):
-        # 微分を初期化
-        self.grad = None
+def rosenrock(x0, x1):
+    y = 100 * (x1 - x0**2) ** 2 + (x0 - 1) ** 2
+    return y
 
 
-class Function:
-    # 関数
-    def __call__(self, *inputs):
-        xs = [x.data for x in inputs]
-        ys = self.forward(*xs)
-        if not isinstance(ys, tuple):
-            ys = (ys,)  # 1要素のタプルに変換する。
-        outputs = [Variable(as_array(y)) for y in ys]
+# x = Variable(np.array(2.0))
+# y = x ** 2
+# y.backward(create_graph=True)
+# gx = x.grad
+# print(gx)
+# gx.name = 'gx'
+# x.cleargrad()
 
-        self.generation = max([x.generation for x in inputs])
-        for output in outputs:
-            output.set_creator(self) # 出力変数に生みの親を覚えさせる。
-        self.inputs = inputs
-        self.outputs = outputs # 出力も覚える。
-        return outputs if len(outputs) > 1 else outputs[0]
-    
-    def forward(self, x):
-        # Functionクラスのforwardクラスを使った人に、継承先で実装すべきなことを教えてあげる
-        raise NotImplementedError()
-    
-    def backward(self, gy):
-        # 逆伝播
-        raise NotImplementedError()
+# z = gx ** 3 + y
+# z.backward()
+# x.name = 'x'
+# y.name = 'y'
+# z.name = 'z'
+# print(x.grad)
+# plot_dot_graph(z, verbose=False, to_file=f'samle.png')
 
-class Exp(Function):
-    def forward(self, x):
-        return np.exp(x)
-    
-    def backward(self, gy):
-        x = self.input.data
-        gx = np.exp(x) * gy
-        return gx
+# x = Variable(np.array(3.0))
+# y = F.sin(x)
+# y.backward(create_graph=True)
+# gx = x.grad
 
-class Square(Function):
-    def forward(self, x):
-        return x ** 2
-    
-    def backward(self, gy):
-        x = self.inputs[0].data
-        gx = 2 * x * gy
-        return gx
+# x.cleargrad()
+# gx.backward(create_graph=True)
+# gx2 = x.grad
+# gx.name = 'gx'
+# x.name = 'x'
+# y.name ='y'
+# plot_dot_graph(gx2, verbose=False, to_file=f'sin.png')
 
-def square(x):
-    return Square()(x)
+x = Variable(np.array(3.0))
+y = F.tanh(x)
+x.name = 'x'
+y.name = 'y'
+y.backward(create_graph=True)
+gx_list = []
+for iters in range(3):
+    for i in range(iters):
+        gx = x.grad
+        x.cleargrad()
+        gx.backward(create_graph=True)
+        gx.name = 'gx' + str(i)
+        gx_list.append(gx)
 
-def exp(x):
-    return Exp()(x)
+    gxn = x.grad
+    gxn.name = 'gx' + str(iters+1)
+    plot_dot_graph(gxn, verbose=False, to_file=f'tanh_{iters}.png')
 
-def numerical_diff(f, x, eps=1e-4):
-    x0 = Variable(np.asarray(x.data - eps))
-    x1 = Variable(np.asarray(x.data + eps))
-    y0 = f(x0)
-    y1 = f(x1)
-    return (y1.data - y0.data) / (2 * eps)
+
+
+
+
+
+
+
+
+
+
+
+
+# def numerical_diff(f, x, eps=1e-4):
+#     x0 = Variable(np.asarray(x.data - eps))
+#     x1 = Variable(np.asarray(x.data + eps))
+#     y0 = f(x0)
+#     y1 = f(x1)
+#     return (y1.data - y0.data) / (2 * eps)
 
